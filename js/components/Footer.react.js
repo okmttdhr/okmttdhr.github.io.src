621fbb214787f9adc507fdac85c26c64e648f8f2
okmttdhr <10101114fin@gmail.com>
2016-01-23 14:54:24 +0700

    c

--- js/components/Footer.react.js
+++ js/components/Footer.react.js
@@ -3,13 +3,37 @@ import { Link } from 'react-router';
 import { connect } from 'react-redux';
 
 export default class Footer extends Component {
+  constructor () {
+    super();
+    this.links = [
+      {
+        label: '>> home',
+        to: '/',
+      },
+      {
+        label: '>> traveling',
+        to: '/travel',
+      },
+      {
+        label: '>> background',
+        to: '/about',
+      }
+    ];
+  }
+
   render() {
-    console.log(this.props);
     return (
       <div className='Footer baseContent'>
-        <p><Link className='linkToPage' to='/'>>> home</Link></p>
-        <p><Link className='linkToPage' to='travel'>>> traveling</Link></p>
-        <p><Link className='linkToPage' to='about'>>> background</Link></p>
+        {
+          this.links.map((link, index) => {
+            if (link.to === this.props.router.location.pathname) return;
+            return <p key={index}>
+              <Link className='linkToPage' to={link.to}>
+                {link.label}
+              </Link>
+            </p>;
+          })
+        }
       </div>
     );
   }
