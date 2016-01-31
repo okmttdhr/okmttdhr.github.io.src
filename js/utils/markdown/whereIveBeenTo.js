fe7fbb0c558d87407d6a0afa6d66ef65bb784f5d
okmttdhr <10101114fin@gmail.com>
2016-01-31 18:52:13 +0700

    b

--- js/utils/markdown/whereIveBeenTo.js
+++ js/utils/markdown/whereIveBeenTo.js
@@ -1,1 +1,1 @@
-export const whereIveBeenTo = `I\'ve been to Australia, Brazil, Okinawa, Finland, Bali Island, Taiwan, Phuket, and Bangkok.`;
+export const whereIveBeenTo = `I\'ve been to Australia, Brazil, Okinawa, Finland, Bali Island, Taiwan, Phuket, Bangkok, and Ko Lanta.`;
