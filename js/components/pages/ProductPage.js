c807abb2831fccf47b4252e58bf2500231936c55
okmttdhr <10101114fin@gmail.com>
2016-07-16 01:13:07 +0900

    update

--- js/components/pages/ProductPage.js
+++ js/components/pages/ProductPage.js
@@ -17,6 +17,26 @@ export default class ProductPage extends Component {
         label: '3D Fire',
         to: 'https://github.com/okmttdhr/threejs-fire',
       },
+      {
+        label: 'Nikkei Hacker',
+        to: 'https://github.com/okmttdhr/nikkei-hacker',
+      },
+      {
+        label: 'Meteor React Redux Boilerplate',
+        to: 'https://github.com/okmttdhr/meteor-react-redux-boilerplate',
+      },
+      {
+        label: 'Rich Text to Markdown',
+        to: 'https://github.com/okmttdhr/rich-text-to-markdown',
+      },
+      {
+        label: 'Checklist for Remote Working',
+        to: 'https://github.com/okmttdhr/remote-work-check-list',
+      },
+      {
+        label: 'esa Bug Fix',
+        to: 'https://github.com/okmttdhr/esa-bug-fix',
+      },
     ];
   }
 
