bb511a1d15ae40d7efd4ad473e2364d5c18db919
okmttdhr <10101114fin@gmail.com>
2016-10-22 17:56:53 +0900

    update

--- js/utils/markdown/whereIveBeenTo.js
+++ js/utils/markdown/whereIveBeenTo.js
@@ -1,1 +1,1 @@
-export const whereIveBeenTo = `I\'ve been to Australia, Brazil, Okinawa, Finland, Bali, Taiwan, Phuket, Bangkok, and Ko Lanta. See all photos here <a class="SocialBtns__btn SocialBtns__btn--ig" href="https://www.instagram.com/okmttdhr/" target="_blank"></a> .`;
+export const whereIveBeenTo = `I\'ve been to Australia, Brazil, Finland, Bali, Taiwan, Phuket, Bangkok, and Ko Lanta. See all photos here <a class="SocialBtns__btn SocialBtns__btn--ig" href="https://www.instagram.com/okmttdhr/" target="_blank"></a> .`;
