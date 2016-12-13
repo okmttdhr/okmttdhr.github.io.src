863e1697e43fa7567f9e9d83b112f521bf8b8b7c
okmttdhr <10101114fin@gmail.com>
2016-12-13 18:36:59 +0900

    update products

--- js/values/product.js
+++ js/values/product.js
@@ -1,42 +1,42 @@
 export const productLinks = [
   {
-    label: 'nativescript-webview-custom-header',
+    label: 'nativescript-webview-custom-header (NativeScript)',
     to: 'https://github.com/okmttdhr/nativescript-webview-custom-header',
   },
   {
-    label: 'Setsuna',
+    label: 'Setsuna (Redux, Firebase)',
     to: 'https://github.com/okmttdhr/setsuna-firebase',
   },
   {
-    label: '3D Fire',
+    label: '3D Fire (three.js)',
     to: 'https://github.com/okmttdhr/threejs-fire',
   },
   {
-    label: 'Cu-hacker',
-    to: 'https://cu-hacker.com/',
-  },
-  {
-    label: 'Nikkei Hacker',
-    to: 'https://github.com/okmttdhr/nikkei-hacker',
-  },
-  {
-    label: 'Meteor React Redux Boilerplate',
+    label: 'Meteor React Redux Boilerplate (Redux, Meteor)',
     to: 'https://github.com/okmttdhr/meteor-react-redux-boilerplate',
   },
   {
-    label: 'Rich Text to Markdown',
-    to: 'https://github.com/okmttdhr/rich-text-to-markdown',
-  },
-  {
-    label: 'Checklist for Remote Working',
+    label: 'Checklist for Remote Working (Remote Working)',
     to: 'https://github.com/okmttdhr/remote-work-check-list',
   },
   {
-    label: 'esa Bug Fix',
+    label: 'Nikkei Hacker (webpack)',
+    to: 'https://github.com/okmttdhr/nikkei-hacker',
+  },
+  {
+    label: 'Rich Text to Markdown (Reactuate)',
+    to: 'https://github.com/okmttdhr/rich-text-to-markdown',
+  },
+  {
+    label: 'esa Bug Fix (UI)',
     to: 'https://github.com/okmttdhr/esa-bug-fix',
   },
   {
-    label: 'I HAV.',
+    label: 'Cu-hacker (Ruby on Rails, jQuery)',
+    to: 'https://cu-hacker.com/',
+  },
+  {
+    label: 'I HAV. (CakePHP, jQuery, CSS, UI)',
     to: 'https://ihav.it/',
   },
 ];
