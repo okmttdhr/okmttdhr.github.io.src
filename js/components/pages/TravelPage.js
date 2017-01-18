83e722d3b44ee3cf19d9628f5c53301f4a8c4d30
okmttdhr <10101114fin@gmail.com>
2017-01-18 21:29:48 +0800

    u

--- js/components/pages/TravelPage.js
+++ js/components/pages/TravelPage.js
@@ -30,6 +30,7 @@ class TravelPage extends Component {
       <div className='TravelPage'>
         <div className='TravelPage__content'>
           <MarkDown value={utilsMarkdown.whereIveBeenTo} />
+          <iframe className='TravelPage__MyMap' src="https://www.google.com/maps/d/u/0/embed?mid=12loqgLDcjQBqe5VMYi_M0fu9chw"></iframe>
           {
             pictures.map((pic, index) => {
               return (<div key={index}>
