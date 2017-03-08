e04149cdb4547e85722fa503647718ddfcfbb872
okmttdhr <10101114fin@gmail.com>
2017-03-08 19:10:50 +0800

    update

--- js/values/product.js
+++ js/values/product.js
@@ -4,12 +4,12 @@ export const productLinksCode = [
     to: 'https://github.com/okmttdhr/YoutuVote',
   },
   {
-    label: 'Setsuna (React, Redux, Firebase)',
-    to: 'https://github.com/okmttdhr/setsuna-firebase',
+    label: 'DokushoPlan (TypeScript, React, AVA, Rollup)',
+    to: 'https://github.com/okmttdhr/DokushoPlan',
   },
   {
-    label: 'nativescript-webview-custom-header (NativeScript)',
-    to: 'https://github.com/okmttdhr/nativescript-webview-custom-header',
+    label: 'Setsuna (React, Redux, Firebase)',
+    to: 'https://github.com/okmttdhr/setsuna-firebase',
   },
   {
     label: '3D Fire (three.js)',
@@ -18,6 +18,10 @@ export const productLinksCode = [
   {
     label: 'Checklist for Remote Working (Remote Working)',
     to: 'https://github.com/okmttdhr/remote-work-check-list',
+  },
+  {
+    label: 'nativescript-webview-custom-header (TypeScript, NativeScript)',
+    to: 'https://github.com/okmttdhr/nativescript-webview-custom-header',
   },
   {
     label: 'Meteor React Redux Boilerplate (React, Redux, Meteor)',
@@ -51,8 +55,16 @@ export const productLinksArticle = [
     to: 'http://blog.mmmcorp.co.jp/blog/2016/12/22/JavaScript/',
   },
   {
+    label: 'フロントエンドのテストに真面目に向き合う',
+    to: 'http://qiita.com/okmttdhr/items/cc58e83c259aa0049538',
+  },
+  {
     label: 'RxJSを例とした独学の10ステップ',
     to: 'http://blog.mmmcorp.co.jp/blog/2017/01/25/learn-rxjs/',
+  },
+  {
+    label: '2016年JSハイブリッドアプリの動向をまとめてみた',
+    to: 'http://blog.mmmcorp.co.jp/blog/2016/04/08/hybrid-app/',
   },
   {
     label: 'レガシーコード改善ガイド内容メモ',
@@ -63,23 +75,23 @@ export const productLinksArticle = [
     to: 'http://blog.mmmcorp.co.jp/blog/2016/11/24/NativeScriptPushNotification/',
   },
   {
-    label: 'ブラジルで時差のあるリモートワーク',
-    to: 'http://blog.mmmcorp.co.jp/blog/2016/08/30/brazil/',
-  },
-  {
     label: 'RxJS雑感',
     to: 'http://blog.mmmcorp.co.jp/blog/2016/08/05/firstimpressionsofrxjs/',
-  },
-  {
-    label: '2016年JSハイブリッドアプリの動向をまとめてみた',
-    to: 'http://blog.mmmcorp.co.jp/blog/2016/04/08/hybrid-app/',
   },
   {
     label: 'NativeScript開発でハマった時の対応まとめ',
     to: 'http://blog.mmmcorp.co.jp/blog/2016/06/10/ns/',
   },
   {
+    label: 'ブラジルで時差のあるリモートワーク',
+    to: 'http://blog.mmmcorp.co.jp/blog/2016/08/30/brazil/',
+  },
+  {
     label: 'ランタ島でのリモートワークレポートと、ロングステイについて',
     to: 'http://blog.mmmcorp.co.jp/blog/2016/03/11/lanta/',
+  },
+  {
+    label: 'プーケットでのリモートワークレポート',
+    to: 'http://blog.mmmcorp.co.jp/blog/2015/12/26/phuket-work-cation/',
   },
 ];
---
+++ test/components/pages/TravelPage.test.js
@@ -0,0 +1,9 @@
+// import expect from 'expect';
+// import { TravelPage } from '../../../js/components/pages/TravelPage';
+//
+// describe('AppActions', () => {
+//   it('^^', () => {
+//     console.log(TravelPage);
+//     expect(true).toBe(true);
+//   });
+// });
